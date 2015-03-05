cb5a2f076d6781c517d9e6eb2b5a1a03ac0a0c57
Alvaro Carvajal <nakotoffana@gmail.com>
2015-03-05 17:44:31 +0100

    .

--- config/connections.js
+++ config/connections.js
@@ -44,7 +44,7 @@ module.exports.connections = {
     adapter: 'sails-mysql',
     host: 'localhost',
     user: 'root',
-    password: '****',
+    password: 'djramos1',
     database: 'aac'
   },
 
